838012c811189cca8872660b6f9dad5c37e4fdcb
Jing Xuan Goh <jingxuan.goh@capbay.com>
2022-08-16 21:28:05 +0800

    payment email template done

--- app/Mail/PaymentEmail.php
+++ app/Mail/PaymentEmail.php
@@ -32,6 +32,28 @@ class PaymentEmail extends Mailable
     {
         $subject = 'Complete Your YJ Art Journal Order';
 
-        return $this->view('emails.payment')->subject($subject)->with([ 'test_message' => $this->data['message'] ]);;
+        return $this->view('emails.payment')->subject($subject)->with([
+            'paymentOption' => 'Bank Transfer',
+            'name' => 'tester',
+            'amount' => number_format(70, 2),
+            'products' => [[
+                "id" => "5ijVADxXLTqLqYwVpJelh5",
+                "quantity" => 1,
+                "totalPrice" => 25.2
+            ]],
+            "totalAmount" => 25.2,
+            "totalDiscount" => 5,
+            "shippingFee" => 8,
+            "totalAfterDiscount" => 28.2,
+            "note" => 'testing',
+            'receiverName' => 'test receiver',
+            'phoneNo' => '1234567',
+            'addressLine1' => '1, Lorong Test 2',
+            'addressLine2' => 'Taman Test',
+            'postcode' => 12300,
+            'city' => 'test city',
+            'state' => 'Selangor',
+            'country' => 'Malaysia'
+        ]);
     }
 }
--- resources/views/emails/payment.blade.php
+++ resources/views/emails/payment.blade.php
@@ -10,33 +10,117 @@
     <img src="https://yj-artjournal-api.herokuapp.com/icon.png" class="center" style="height: 150px; width: 150px;" />
     {{-- src={{asset('icon.png')}} --}}
     <hr class="thick-divider">
-    <h2 class="customFont">
-      Pay via {{$paymentOption}}
-    </h2>
-    <p class="customFont">Hi {{ $name }},</p>
-    <p class="customFont">
+    <h2>Pay via {{$paymentOption}}</h2>
+    <p>Hi {{ $name }},</p>
+    <p>
       Please kindly proceed to your payment of <strong>RM {{$amount}}</strong> via
       {{$paymentOption}} to:
     </p>
     <hr class="thin-divider">
     @if($paymentOption==='Bank Transfer')
-    <h2 class="customFont center" style="font-weight: normal;text-align:center">
+    <h2 class="center-thin-h2">
       CIMB <br />7061620467<br />Chiah Yi Jie
     </h2>
     @else
-    <img src="https://yj-artjournal-api.herokuapp.com/TNG.JPG" class="center" style="height: 150px; width: 150px;" />
+    <img src="https://yj-artjournal-api.herokuapp.com/TNG.JPG" class="center" style="width: 450px;" />
     @endif
     <hr class="thin-divider">
+    <p>
+      Reminder* <br />
+      Payment has to be done within 24 hrs for your order to be processed, or your order will be cancelled.
+    </p>
+    <table class="product-table">
+      <tr>
+        <th>Item</th>
+        <th>Qty</th>
+        <th>Price (RM)</th>
+      </tr>
+      @foreach ($products as $product)
+      <tr class="product">
+        <td>{{$product['id']}}</td>
+        <td class="center">{{$product['quantity']}}</td>
+        <td class="currency-align">{{$product['totalPrice']}}</td>
+      </tr>
+      @endforeach
+      <tr>
+        <td><strong>Total Amount</strong></td>
+        <td colspan="2" class="currency-align"><strong>{{$totalAmount}}</strong></td>
+      </tr>
+      <tr>
+        <td class="green-text"><strong>Discount</strong></td>
+        <td colspan="2" class="currency-align green-text"><strong>-{{$totalDiscount}}</strong></td>
+      </tr>
+      <tr>
+        <td><strong>Shipping Fee</strong></td>
+        <td colspan="2" class="currency-align"><strong>{{$shippingFee}}</strong></td>
+      </tr>
+      <tr>
+        <td><strong>Total After Discount</strong></td>
+        <td colspan="2" class="currency-align"><strong>{{$totalAfterDiscount}}</strong></td>
+      </tr>
+    </table>
+    <p>
+      <span style="font-weight: 600">Note to Seller:</span> <br /> {{$note}}
+    </p>
+    <p>
+      We will verify your payment and send a receipt via email shortly.<br />
+      Free feel to contact me at <a href="mailto:hello@yjartjournal.com">hello@yjartjournal.com</a>.
+    </p>
+    <h2 class="center-thin-h2">Have A Great Day!</h2>
+    <p style="font-style: italic;margin-top:0px" class="center">
+      "When you buy from a small business, an actual person does a little happy dance."
+    </p>
+    <p>Regards<br />YJ Art Journal</p>
+    <hr class="thick-divider">
+    <p style="font-weight:bold">Delivery Details</p>
+    <table>
+      <tr>
+        <td>Name:</td>
+        <td>{{$receiverName}}</td>
+      </tr>
+      <tr>
+        <td>Contact No:</td>
+        <td>{{$phoneNo}}</td>
+      </tr>
+      <tr>
+        <td>Address:</td>
+        <td>{{$addressLine1}} {{$addressLine2}} {{$postcode}} {{$city}} {{$state}} {{$country}}</td>
+      </tr>
+    </table>
+    <hr class="thick-divider">
   </div>
 </body>
 
 <style>
   .center {
-    align-self: center
+    align-self: center;
+    text-align: center
   }
 
-  .customFont {
+  p {
     font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+
+  h2 {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+
+  table {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+
+  .product-table td,
+  th {
+    border: 1px solid #F5DBC9;
+    padding: 8px;
+  }
+
+  .product:nth-child(even) {
+    background-color: #F5DBC9;
+  }
+
+  .currency-align {
+    text-align: right
   }
 
   .thick-divider {
@@ -51,6 +135,15 @@
     border-radius: 5px;
     width: 100%;
     margin-top: 20px;
+  }
+
+  .green-text {
+    color: green
+  }
+
+  .center-thin-h2 {
+    font-weight: normal;
+    text-align: center
   }
 </style>
 
